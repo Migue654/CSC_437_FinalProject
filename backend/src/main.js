bc44975e5b4113dcf35723b4ed340115771b2d4e
Migue654 <mmedin52@calpoly.edu>
2026-03-20 16:35:32 -0700

    Deployment of Final-rm-shared-test

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getEnvVar } from "./getEnvVar.js";
-import { SHARED_TEST } from "./shared/example.js";
+
 import { connectMongo } from "./connectMongo.js";
 import { UserProvider } from "./UserProvider.js";
 import { registerAuthRoutes } from "./routes/authRoutes.js";
@@ -26,9 +26,6 @@ registerAuthRoutes(app, userProvider);
 registerPantryRoutes(app, pantryProvider);
 registerMealRoutes(app, mealProvider);
 
-app.get("/hello", (req, res) => {
-    res.send("Hello, World " + SHARED_TEST);
-});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}.  CTRL+C to stop.`);
