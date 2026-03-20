776f31ca3c21f2587ca1e4085ad66cea25b861ad
Migue654 <mmedin52@calpoly.edu>
2026-03-20 16:42:03 -0700

    Deployment of Final-VPS-troubleshoot

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { getEnvVar } from "./getEnvVar.js";
-
 import { connectMongo } from "./connectMongo.js";
 import { UserProvider } from "./UserProvider.js";
 import { registerAuthRoutes } from "./routes/authRoutes.js";
@@ -9,10 +8,12 @@ import { PantryProvider } from "./PantryProvider.js";
 import { MealProvider } from "./MealProvider.js";
 import { registerMealRoutes } from "./routes/mealRoutes.js";
 
-
 const PORT = Number.parseInt(getEnvVar("PORT", false), 10) || 3000;
+const STATIC_DIR = getEnvVar("STATIC_DIR", false) || "public";
+
 const app = express();
 app.use(express.json());
+app.use(express.static(STATIC_DIR));
 
 const mongoClient = connectMongo();
 await mongoClient.connect();
@@ -26,6 +27,10 @@ registerAuthRoutes(app, userProvider);
 registerPantryRoutes(app, pantryProvider);
 registerMealRoutes(app, mealProvider);
 
+const VALID_ROUTES = ["/", "/login", "/register", "/supply", "/ideas", "/settings"];
+app.get(VALID_ROUTES, (req, res) => {
+    res.sendFile("index.html", { root: STATIC_DIR });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}.  CTRL+C to stop.`);
